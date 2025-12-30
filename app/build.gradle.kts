74874a161d44c5254ade0e3b2a1b56e1c480e434
rumeysazeynep <rumeysazeynepkabadayi@hotmail.com>
2025-12-30 21:42:50 +0300

    Created view binding logic and added design some pages.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,10 @@ android {
         version = release(36)
     }
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.contactsapp"
         minSdk = 24
